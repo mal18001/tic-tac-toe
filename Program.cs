fe94a24f389bbf8992cb147ffad68b2162d673b6
cmaldo16 <cmaldo16@gmail.com>
2022-09-23 01:52:35 -0600

    Very close to being done, just a little more

--- Program.cs
+++ Program.cs
@@ -1,52 +1,102 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+class Program
+{
+    public void check(int num, int[] x_used) {
 
-// bool first = true;
-// bool second = true;
-// bool third = true;
-// bool fourth = true;
-// bool fifth = true;
-// bool sixth = true;
-// bool seventh = true;
-// bool eighth = true;
-// bool ninth = true;
+    }
 
-// string board1 = "     |     |";
-// string board2 = "  1  |  2  |  3";
-// string board3 = "-----|-----|-----";
-// string board4 = "  4  |  5  |  6";
-// string board5 = "-----|-----|-----";
-// string board6 = "  7  |  8  |  9";
-// string board7 = "     |     |";
 
-string x_turn = "X's turn to choose a square:";
-string o_turn = "O's turn to choose a square:";
+    static void Main(string[] args) {
 
-bool win = false;
+        Program pr = new Program();
+        string[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-while (win == false) {
-    Console.WriteLine($"\n {numbers[0]} | {numbers[1]} | {numbers[2]} \n---|---|---\n {numbers[3]} | {numbers[4]} | {numbers[5]} \n---|---|---\n {numbers[6]} | {numbers[7]} | {numbers[8]}");
-    Console.Write($"\n{x_turn} ");
-    int x_move = Console.ReadLine();
-    int x_space_chosen = x_move - 1;
-    numbers[x_space_chosen] = "X";
-    int i = 0;
-    foreach (string number in numbers) {
-        List<int> check = new List<int>();
-        i++;
-        if (number == "X") {
-            check.Add(i);
+        string x_turn = "X's turn to choose a square:";
+        string o_turn = "O's turn to choose a square:";
+
+        int[] x_used = {};
+        int[] o_used = {};
+        int[] both_used = {};
+
+        bool x_win = false;
+        bool o_win = false;
+
+        while (x_win == false && o_win == false) {
+            Console.WriteLine($"\n {numbers[0]} | {numbers[1]} | {numbers[2]} \n---|---|---\n {numbers[3]} | {numbers[4]} | {numbers[5]} \n---|---|---\n {numbers[6]} | {numbers[7]} | {numbers[8]}");
+            Console.Write($"\n{x_turn} ");
+
+            int x_move = Convert.ToInt32(Console.ReadLine());
+            int x_space_chosen = x_move - 1;
+
+            numbers[x_space_chosen] = "X";
+
+            x_used = x_used.Append(x_move).ToArray();
+            Array.Sort(x_used);
+            int first_x = x_used[0];
+
+            foreach (int num in x_used) {
+                if (first_x + 1 == num){
+//                    pr.check(num, x_used);
+                    foreach (int num2 in x_used){
+                        if (first_x + 2 == num2) {
+                            x_win = true;
+                        }
+                    }
+                } 
+                else if (first_x + 3 == num){
+                    foreach (int num2 in x_used){
+                        if (first_x + 2 == num2) {
+                            x_win = true;
+                        }
+                    }
+                } 
+                else if (first_x + 4 == num) {
+                    foreach (int num2 in x_used){
+                        if (first_x + 2 == num2) {
+                            x_win = true;
+                        }
+                    }
+                }
+            }
         }
-    }
 
-    }
-    
-    while (win == false) {
-        Console.WriteLine($"\n {numbers[0]} | {numbers[1]} | {numbers[2]} \n---|---|---\n {numbers[3]} | {numbers[4]} | {numbers[5]} \n---|---|---\n {numbers[6]} | {numbers[7]} | {numbers[8]}");
-        Console.Write($"\n{o_turn} ");
-        int o_move = Console.ReadLine();
-        int o_space_chosen = x_move - 1;
-        numbers[o_space_chosen] = "O";
-    }
+            while (x_win == false && o_win == false) {
+                Console.WriteLine($"\n {numbers[0]} | {numbers[1]} | {numbers[2]} \n---|---|---\n {numbers[3]} | {numbers[4]} | {numbers[5]} \n---|---|---\n {numbers[6]} | {numbers[7]} | {numbers[8]}");
+                Console.Write($"\n{o_turn} ");
 
-}
+                int o_move = Convert.ToInt32(Console.ReadLine());
+                int o_space_chosen = o_move - 1;
+
+                numbers[o_space_chosen] = "O";
+
+                o_used = o_used.Append(o_move).ToArray();
+                Array.Sort(o_used);
+                int first_o = o_used[0];
+
+                foreach (int num in o_used) {
+                    if (first_o + 1 == num){
+//                        pr.check(num, o_used);
+                        foreach (int num2 in o_used){
+                            if (first_o + 2 == num2) {
+                                o_win = true;
+                            }
+                        }
+                    } 
+                    else if (first_o + 3 == num){
+                        foreach (int num2 in o_used){
+                            if (first_o + 2 == num2) {
+                                o_win = true;
+                            }
+                        }
+                    } 
+                    else if (first_o + 4 == num) {
+                        foreach (int num2 in o_used){
+                            if (first_o + 2 == num2) {
+                                o_win = true;
+                            }
+                        }
+                    }
+                }
+            }
+    }   
+}   

a596a7762c930e6980be8ea5142141f03c0e81a6
cmaldo16 <cmaldo16@gmail.com>
2022-09-23 02:55:44 -0600

    Debugging and finishing touches!

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ class Program
     static void Main(string[] args) {
 
         Program pr = new Program();
+        string[] x_and_o = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
         string[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
         string x_turn = "X's turn to choose a square:";
@@ -20,21 +21,36 @@ class Program
 
         bool x_win = false;
         bool o_win = false;
+        bool no_win = false;
 
-        while (x_win == false && o_win == false) {
-            Console.WriteLine($"\n {numbers[0]} | {numbers[1]} | {numbers[2]} \n---|---|---\n {numbers[3]} | {numbers[4]} | {numbers[5]} \n---|---|---\n {numbers[6]} | {numbers[7]} | {numbers[8]}");
+        while (x_win == false && o_win == false && no_win == false) {
+            Console.WriteLine("\nPlaying Board\tReference Board\n");
+            Console.WriteLine($"  {x_and_o[0]} | {x_and_o[1]} | {x_and_o[2]} \t   {numbers[0]} | {numbers[1]} | {numbers[2]}\n ---|---|--- \t  ---|---|---\n  {x_and_o[3]} | {x_and_o[4]} | {x_and_o[5]} \t   {numbers[3]} | {numbers[4]} | {numbers[5]}");
+            Console.WriteLine($" ---|---|--- \t  ---|---|---\n  {x_and_o[6]} | {x_and_o[7]} | {x_and_o[8]} \t   {numbers[6]} | {numbers[7]} | {numbers[8]}");
             Console.Write($"\n{x_turn} ");
 
             int x_move = Convert.ToInt32(Console.ReadLine());
             int x_space_chosen = x_move - 1;
 
-            numbers[x_space_chosen] = "X";
+            x_and_o[x_space_chosen] = "X";
+            numbers[x_space_chosen] = "/";
 
             x_used = x_used.Append(x_move).ToArray();
             Array.Sort(x_used);
             int first_x = x_used[0];
 
-            foreach (int num in x_used) {
+            both_used = both_used.Append(x_move).ToArray();
+            Array.Sort(both_used);
+            int last = both_used[both_used.Length-1];
+
+            if (last == 9) {
+                no_win = true;
+            }
+
+            Console.WriteLine("\n\n\n");
+
+            foreach (int i in x_used) {
+                int num = i + 1;
                 if (first_x + 1 == num){
 //                    pr.check(num, x_used);
                     foreach (int num2 in x_used){
@@ -45,37 +61,44 @@ class Program
                 } 
                 else if (first_x + 3 == num){
                     foreach (int num2 in x_used){
-                        if (first_x + 2 == num2) {
+                        if (first_x + 6 == num2) {
                             x_win = true;
                         }
                     }
                 } 
                 else if (first_x + 4 == num) {
                     foreach (int num2 in x_used){
-                        if (first_x + 2 == num2) {
+                        if (first_x + 8 == num2) {
                             x_win = true;
                         }
                     }
                 }
             }
-        }
 
-            while (x_win == false && o_win == false) {
-                Console.WriteLine($"\n {numbers[0]} | {numbers[1]} | {numbers[2]} \n---|---|---\n {numbers[3]} | {numbers[4]} | {numbers[5]} \n---|---|---\n {numbers[6]} | {numbers[7]} | {numbers[8]}");
+            if (x_win == false && o_win == false && no_win == false) {
+                Console.WriteLine("\nPlaying Board\tReference Board\n");
+                Console.WriteLine($"  {x_and_o[0]} | {x_and_o[1]} | {x_and_o[2]} \t   {numbers[0]} | {numbers[1]} | {numbers[2]}\n ---|---|--- \t  ---|---|---\n  {x_and_o[3]} | {x_and_o[4]} | {x_and_o[5]} \t   {numbers[3]} | {numbers[4]} | {numbers[5]}");
+                Console.WriteLine($" ---|---|--- \t  ---|---|---\n  {x_and_o[6]} | {x_and_o[7]} | {x_and_o[8]} \t   {numbers[6]} | {numbers[7]} | {numbers[8]}");
                 Console.Write($"\n{o_turn} ");
 
                 int o_move = Convert.ToInt32(Console.ReadLine());
                 int o_space_chosen = o_move - 1;
 
-                numbers[o_space_chosen] = "O";
+                x_and_o[o_space_chosen] = "O";
+                numbers[o_space_chosen] = "/";
 
                 o_used = o_used.Append(o_move).ToArray();
                 Array.Sort(o_used);
                 int first_o = o_used[0];
 
-                foreach (int num in o_used) {
+                both_used = both_used.Append(x_move).ToArray();
+                Array.Sort(both_used);
+
+                Console.WriteLine("\n\n\n");
+
+                foreach (int i in o_used) {
+                    int num = i + 1;
                     if (first_o + 1 == num){
-//                        pr.check(num, o_used);
                         foreach (int num2 in o_used){
                             if (first_o + 2 == num2) {
                                 o_win = true;
@@ -84,19 +107,19 @@ class Program
                     } 
                     else if (first_o + 3 == num){
                         foreach (int num2 in o_used){
-                            if (first_o + 2 == num2) {
+                            if (first_o + 6 == num2) {
                                 o_win = true;
                             }
                         }
                     } 
                     else if (first_o + 4 == num) {
                         foreach (int num2 in o_used){
-                            if (first_o + 2 == num2) {
+                            if (first_o + 8 == num2) {
                                 o_win = true;
                             }
                         }
                     }
                 }
-            }
+        }   }
     }   
 }   
